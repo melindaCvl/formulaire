f6bb7e1876a9fdfd723cd3241674c16762d38849
antoi <Olex0201>
2019-04-07 22:28:53 +0200

    si erreur renvoi info au form + message d'erreur + si ok message OK

--- formulaireMVC/webapp-template/src/main/java/formulaire/FormulaireServlet.java
+++ formulaireMVC/webapp-template/src/main/java/formulaire/FormulaireServlet.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -22,27 +24,44 @@ public class FormulaireServlet extends HttpServlet {
 
         // Récupération des champs (d'une manière ou d'une autre)
         String email = req.getParameter("email");
-        String mdp = req.getParameter("password");
-        String mdpBis = req.getParameter("passwordBis");
+        String password = req.getParameter("password");
+        String passwordBis = req.getParameter("passwordBis");
         String approbation = req.getParameter("approbation");
+
         try {
-            this.verifierChamps(email, mdp, mdpBis, approbation);
-            // Réussite
 
-/** TODO afficher message si verifCHamp OK manque a retourné boolean ? **/
-            DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
-            DateFormat timeFormat = new SimpleDateFormat("HH:mm:ss");
-            Date date = new Date();
+            if (this.testChamps(email, password, passwordBis, approbation)) {
+                Date today = new Date();
+                DateFormat fullDateFormat = DateFormat.getDateTimeInstance(DateFormat.FULL, DateFormat.MEDIUM);
 
-            resp.setContentType("html");
-            resp.setCharacterEncoding("utf-8");
-            resp.getWriter().write("Votre inscription a bien été prise en compte le " + dateFormat.format(date)
-                    + " à " + dateFormat.format(timeFormat) + " pour l'adresse mail " + email + " .");
+                resp.setContentType("text/html");
+                resp.setCharacterEncoding("utf-8");
+                resp.getWriter().write("Votre inscription a bien été prise en compte le " + fullDateFormat.format(today)
+                        + " pour l'adresse mail " + email + " ." + approbation);
+            } else {
+                if (this.isEmail(email)) {
+                    req.setAttribute("email", email);
+                } else {
+                    req.setAttribute("errorEmail", "adresse email incorrecte ");
+                }
+                if (!this.isPassword(password)) {
+                    req.setAttribute("errorPassword", "mot de passe incorrecte ");
+                }
+                if (!this.isSamePassword(password, passwordBis)) {
+                    req.setAttribute("errorSamePassword", "les deux mots de passes sont incorrectes ");
+                }
+                if (this.isApprobation(approbation)) {
+                    req.setAttribute("approbation", "checked");
+                } else {
+                    req.setAttribute("errorApprobation", "vous n'avez pas approuvé les conditions générales de ce site");
+                }
+                req.getRequestDispatcher("index.jsp").forward(req, resp);
+            }
 
-        } catch (ExceptionChamp exc) {
-            // Echec - Traiter
-            String msgErreur = exc.getMessage();
+        } catch (Exception e) {
+            resp.getWriter().write("erreur: " + e);
         }
+
     }
 
     /**
@@ -89,4 +108,94 @@ public class FormulaireServlet extends HttpServlet {
             throw new ExceptionChamp("Merci de bien vouloir approuver les conditions générales du site.");
         }
     }
+
+    /**
+     * @param email
+     * @param mdp
+     * @param mdpBis
+     * @param approbation
+     * @return
+     */
+    private boolean testChamps(String email, String mdp, String mdpBis, String approbation) {
+        return isEmail(email) && isPassword(mdp) && isSamePassword(mdp, mdpBis) && isApprobation(approbation);
+    }
+
+
+    /**
+     * start of the line
+     * [_A-Za-z0-9-\\+]+	#  must start with string in the bracket [ ], must contains one or more (+)
+     * (			#   start of group #1
+     * \\.[_A-Za-z0-9-]+	#     follow by a dot "." and string in the bracket [ ], must contains one or more (+)
+     * )*			#   end of group #1, this group is optional (*)
+     *
+     * @param email
+     * @return
+     * @			# must contains a "@" symbol
+     * [A-Za-z0-9-]+      #       follow by string in the bracket [ ], must contains one or more (+)
+     * (			#         start of group #2 - first level TLD checking
+     * \\.[A-Za-z0-9]+  #           follow by a dot "." and string in the bracket [ ], must contains one or more (+)
+     * )*		#         end of group #2, this group is optional (*)
+     * (			#         start of group #3 - second level TLD checking
+     * \\.[A-Za-z]{2,}  #           follow by a dot "." and string in the bracket [ ], with minimum length of 2
+     * )			#         end of group #3
+     * $			#end of the line
+     */
+    private boolean isEmail(String email) {
+        String emailRegex = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+                + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+        Pattern p = Pattern.compile(emailRegex);
+        Matcher m = p.matcher(email);
+        return m.matches();
+    }
+
+    /**
+     * (			# Start of group
+     * (?=.*\d)		#   must contains one digit from 0-9
+     * (?=.*[a-z])		#   must contains one lowercase characters
+     * (?=.*[A-Z])		#   must contains one uppercase characters
+     * (?=.*[@#$%])		#   must contains one special symbols in the list "@#$%"
+     * .		#     match anything with previous condition checking
+     * {6,20}	#        length at least 6 characters and maximum of 20
+     * )			# End of group
+     *
+     * @param password
+     * @return
+     */
+    private boolean isPassword(String password) {
+        String passwordRegex = "((?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%]).{8,})";
+        Pattern p = Pattern.compile(passwordRegex);
+        Matcher m = p.matcher(password);
+        return m.matches();
+    }
+
+
+    /**
+     * @param password
+     * @param passwordBis
+     * @return
+     */
+    private boolean isSamePassword(String password, String passwordBis) {
+        return (password.equals(passwordBis));
+    }
+
+
+    /**
+     * @return
+     */
+    private boolean isApprobation() {
+        return isApprobation();
+    }
+
+    /**
+     * @param approbation
+     * @return
+     */
+    private boolean isApprobation(String approbation) {
+        if (approbation != null) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 }
--- formulaireMVC/webapp-template/src/main/webapp/index.jsp
+++ formulaireMVC/webapp-template/src/main/webapp/index.jsp
@@ -1,4 +1,5 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <html>
 <head>
     <link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
@@ -10,22 +11,29 @@
     <title>Formulaire d'adhésion</title>
 </head>
 <body>
-<h1>Formulaire d'adhésion</h1>
+
+
+
 
 <!-- main -->
 <div class="main-w3layouts wrapper">
-    <h1>Creative SignUp Form</h1>
+    <h1>Formulaire d'adhésion</h1>
     <div class="main-agileinfo">
         <div class="agileits-top">
             <form action="./formulaire" method="post">
-                <input class="text email" type="text" name="email" placeholder="Email" required="">
-                <input class="text" type="text" name="password" placeholder="Password" required="">
-                <input class="text w3lpass" type="text" name="passwordBis" placeholder="Confirm Password" required="">
+                <c:if test = "${not empty errorEmail || not empty errorPassword || not empty errorSamePassword || not empty errorApprobation}">
+                    <div class="warning"><c:out value="${errorEmail} <br/> ${errorPassword} <br/> ${errorSamePassword} <br/> ${errorApprobation}" escapeXml="false"/></div>
+                </c:if>
+<%--                <div class="warning">${errorEmail} ${errorPassword} ${errorSamePassword} ${errorApprobation}</div>--%>
+                <input class="text email" type="text" name="email" placeholder="Email" value="${email}">
+                <input class="text" type="text" name="password" placeholder="Password">
+                <input class="text w3lpass" type="text" name="passwordBis" placeholder="Confirm Password">
                 <div class="wthree-text">
                     <label class="anim">
-                        <input type="checkbox" class="checkbox" name="approbation">
+                        <input type="checkbox" class="checkbox" name="approbation" ${approbation}>
                         <span>I Agree To The Terms & Conditions</span>
                     </label>
+                    <!-- pour les bubules -->
                     <div class="clear"></div>
                 </div>
                 <input type="submit" value="SIGNUP">
@@ -34,7 +42,7 @@
     </div>
     <!-- copyright -->
     <div class="colorlibcopy-agile">
-        <p>© 2018 Colorlib Signup Form. All rights reserved | Design by Mélida CVL & Antoine VL</a>
+        <p>© 2019 TP JAVA EE. All rights reserved | Design by Mélida CVL & Antoine VL</a>
         </p>
     </div>
     <!-- //copyright -->
