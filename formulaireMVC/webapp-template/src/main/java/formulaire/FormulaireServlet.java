68227f94d4bc11e29c0b4b472b0898a90f26a92f
Mélinda Clerville <melinda.coralie.clerville@outlook.com>
2019-04-09 10:47:54 +0200

    Toujours pas

--- formulaireMVC/webapp-template/src/main/java/formulaire/FormulaireServlet.java
+++ formulaireMVC/webapp-template/src/main/java/formulaire/FormulaireServlet.java
@@ -2,19 +2,13 @@ package formulaire;
 
 import java.io.IOException;
 import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import exception.ExceptionChamp;
-import jdk.jshell.execution.Util;
 
 @WebServlet("/formulaire")
 public class FormulaireServlet extends HttpServlet {
@@ -34,35 +28,50 @@ public class FormulaireServlet extends HttpServlet {
         try {
 
             if (u.testChamps(email, password, passwordBis, approbation)) {
-                Date today = new Date();
-                DateFormat fullDateFormat = DateFormat.getDateTimeInstance(DateFormat.FULL, DateFormat.MEDIUM);
-
-                resp.setContentType("text/html");
-                resp.setCharacterEncoding("utf-8");
-                resp.getWriter().write("Votre inscription a bien été prise en compte le " + fullDateFormat.format(today)
-                        + " pour l'adresse mail " + email + " .");
+            	succes(resp, email);
             } else {
-                if (u.emailCorrect(email)) {
-                    req.setAttribute("email", email);
-                } else {
-                    req.setAttribute("errorEmail", "Adresse email incorrecte <br/>");
-                }
-                if (!u.mdpCorrect(password)) {
-                    req.setAttribute("errorPassword", "Mot de passe incorrect <br/>");
-                }
-                if (!u.mdpIdentiques(password, passwordBis)) {
-                    req.setAttribute("errorSamePassword", "Les deux mots de passes sont incorrects <br/>");
-                }
-                if (u.approbationOk(approbation)) {
-                    req.setAttribute("approbation", "checked");
-                } else {
-                    req.setAttribute("errorApprobation", "Vous n'avez pas approuvé les conditions générales de ce site <br/>");
-                }
-                req.getRequestDispatcher("index.jsp").forward(req, resp);
+            	echec(req, resp, u, email, password, passwordBis, approbation);
             }
 
         } catch (Exception e) {
             resp.getWriter().write("erreur: " + e);
         }
+    }
+    
+    private void succes(HttpServletResponse resp, String email) {
+        Date today = new Date();
+        DateFormat fullDateFormat = DateFormat.getDateTimeInstance(DateFormat.FULL, DateFormat.MEDIUM);
+
+        resp.setContentType("text/html");
+        resp.setCharacterEncoding("utf-8");
+        
+        try {
+	        resp.getWriter().write("Votre inscription a bien été prise en compte le " + fullDateFormat.format(today)
+	                + " pour l'adresse mail " + email + " .");
+        } catch (Exception e) {}
+    }
+    
+    private void echec(HttpServletRequest req, HttpServletResponse resp, UtilisateurDAO u, String email, String password, String passwordBis, String approbation) {
+        if (u.emailCorrect(email)) {
+            req.setAttribute("email", email);
+        } else {
+            req.setAttribute("errorEmail", "Adresse email incorrecte <br/>");
+        }
+        if (!u.mdpCorrect(password)) {
+            req.setAttribute("errorPassword", "Mot de passe incorrect <br/>");
+        }
+        if (!u.mdpIdentiques(password, passwordBis)) {
+            req.setAttribute("errorSamePassword", "Les deux mots de passes sont incorrects <br/>");
+        }
+        if (u.approbationOk(approbation)) {
+            req.setAttribute("approbation", "checked");
+        } else {
+            req.setAttribute("errorApprobation", "Vous n'avez pas approuvé les conditions générales de ce site <br/>");
+        }
+        
+        try {
+        	req.getRequestDispatcher("index.jsp").forward(req, resp);
+        } catch (Exception e) {}
+        
     }
 }
